3f20dd785ac0dc478c0534f0c745962566845e90
Aman07a <aman16.aa17@gmail.com>
2023-06-30 15:56:55 +0200

    Adding an Event Listener

--- js/shared.js
+++ js/shared.js
@@ -5,6 +5,10 @@ var selectPlanButtons = document.querySelectorAll(".plan button");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
 
-console.dir(backdrop);
-
-backdrop.style.display = "block";
+// console.dir(backdrop);
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", function () {
+    modal.style.display = "block";
+    backdrop.style.display = "block";
+  });
+}
