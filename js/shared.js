80428341ab2be51fbaef314d745144aa7cedb1ea
Aman07a <aman16.aa17@gmail.com>
2023-06-30 12:16:43 +0200

    Selecting & Manipulating Styles with JavaScript

---
+++ js/shared.js
@@ -0,0 +1,10 @@
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var modalNoButton = document.querySelector(".modal__action--negative");
+var selectPlanButtons = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+
+console.dir(backdrop);
+
+backdrop.style.display = "block";
