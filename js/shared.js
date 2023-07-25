5c672fe320cd9342153f60bc0a427b9582e47587
Aman07a <aman16.aa17@gmail.com>
2023-07-25 12:00:11 +0200

    Manipulating Element Classes

--- js/shared.js
+++ js/shared.js
@@ -8,24 +8,32 @@ var mobileNav = document.querySelector(".mobile-nav");
 // console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function () {
-    modal.style.display = "block";
-    backdrop.style.display = "block";
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // module.className = "open"; // This will actually overwrite the complete class list
+    modal.classList.add("open");
+    backdrop.classList.add("open");
   });
 }
 
 backdrop.addEventListener("click", function () {
-  mobileNav.style.display = "none";
+  // mobileNav.style.display = "none";
+  mobileNav.classList.remove("open");
   closeModal();
 });
 
 modalNoButton.addEventListener("click", closeModal);
 
 function closeModal() {
-  backdrop.style.display = "none";
-  modal.style.display = "none";
+  // backdrop.style.display = "none";
+  // modal.style.display = "none";
+  modal.classList.remove("open");
+  backdrop.classList.remove("open");
 }
 
 toggleButton.addEventListener("click", function () {
-  mobileNav.style.display = "block";
-  backdrop.style.display = "block";
+  // mobileNav.style.display = "block";
+  // backdrop.style.display = "block";
+  mobileNav.classList.add("open");
+  backdrop.classList.add("open");
 });
